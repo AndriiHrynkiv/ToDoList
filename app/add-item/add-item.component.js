d18d40597bf87bb14d456fe7062653e1d9475dda
Andrii.Hrynkiv <Andrii.Hrynkiv@eleks.com>
2017-11-16 12:17:07 +0200

    added phone filter

--- app/add-item/add-item.component.js
+++ app/add-item/add-item.component.js
@@ -9,10 +9,27 @@ component('addItem', {
     } else { $scope.todoList = JSON.parse(localStorage.getItem('todoList'))}
     
       $scope.addData = function () {
-            if ( $scope.personName === undefined || $scope.Phone === undefined || $scope.byTime  === null) {
-                $scope.error ="Please fill all fields";
-                return;
-            } else {
+
+        if ( $scope.personName === undefined || $scope.Phone === undefined || $scope.byTime  === null) {
+            $scope.error ="Please fill all fields";
+            return;
+        } else {$scope.error=""}
+
+     var checPhone = /^(\+|00)(\d|\()(\d|\,|\-|\)|\s){7}(\d|\s)+$/g.test($scope.Phone);
+     var PhoneChekcPart = $scope.Phone.slice(2, 8);
+     var testExpresin = /(\,|\-)/g;
+
+       if ( checPhone === false){
+          $scope.error="Please enter correct phone number";
+          return;
+      } else {$scope.error=""}
+
+      if (PhoneChekcPart.match(testExpresin) && PhoneChekcPart.match(testExpresin).length > 1) {   
+            console.log(PhoneChekcPart.match(test2).length)  ;
+            $scope.error="Please enter correct phone number";
+            return; 
+      } else {$scope.error=""}
+      
               $scope.todoList.push({
                                   personName:$scope.personName, 
                                   Phone:((($scope.Phone).replace(/\+/, "00")).replace(/(\(|\)|-)/g, "")),
@@ -20,7 +37,7 @@ component('addItem', {
                                   Delete:"",
                                   Done:false
                               }); 
-                          }       
+                              
        }
 
          $scope.sortBy = function(propertyName) {
